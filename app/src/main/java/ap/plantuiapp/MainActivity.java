3b55ac2996e3c7e097227e93ce4e5fdd32f20030
Kristof <kristof.vanderlinden@student.ap.be>
2015-03-04 19:23:08 +0100

    PopupMenu

--- app/src/main/java/ap/plantuiapp/MainActivity.java
+++ app/src/main/java/ap/plantuiapp/MainActivity.java
@@ -7,26 +7,40 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
     Button Timerbtn;
     Button Bluetoothbtn;
+    ImageButton HelpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Timerbtn = (Button)findViewById(R.id.ButtonGoToTimer);
+        Timerbtn = (Button) findViewById(R.id.ButtonGoToTimer);
         Timerbtn.setOnClickListener(this);
-        Bluetoothbtn = (Button)findViewById(R.id.BtnGoToBluetooth);
+        Bluetoothbtn = (Button) findViewById(R.id.BtnGoToBluetooth);
         Bluetoothbtn.setOnClickListener(this);
+        HelpButton = (ImageButton) findViewById(R.id.Imghelpbutton);
+        HelpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, HelpButton);
+                popup.getMenuInflater().inflate(R.menu.menu_main, popup.getMenu());
+                popup.show();
 
+            }
+
+
+        });
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu );
         return true;
     }
 

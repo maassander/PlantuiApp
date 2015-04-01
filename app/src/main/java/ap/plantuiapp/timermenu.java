c78760ae9c91a6b558b2c5137ce25e3456747317
Bart Gevaert <bart.gevaert@student.ap.be>
2015-04-01 14:40:18 +0200

    Updated timer

--- app/src/main/java/ap/plantuiapp/timermenu.java
+++ app/src/main/java/ap/plantuiapp/timermenu.java
@@ -7,29 +7,78 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ListAdapter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class timermenu extends ActionBarActivity {
 
-    private String[] monthsArray = { "Mooie plantjes", "Grote plantjes", "Groene plantjes", "Groenere plantjes"};
-
+    private String[] monthsArray = {};
     private ListView monthsListView;
+
+    private ListAdapter listAdapter;
+
     private ArrayAdapter arrayAdapter;
+    private EditText textboxTime1;
+    private EditText textboxTime2;
+    private EditText textboxTime3;
+    private EditText textboxTime4;
+    private String checkTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timermenu);
 
-        monthsListView = (ListView) findViewById(R.id.listofdevices);
+        monthsListView = (ListView) findViewById(R.id.list_ShowSchedules);
         // this-The current activity context.
         // Second param is the resource Id for list layout row item
         // Third param is input array
-       arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, monthsArray);
+        String test = " efzefzef    ";
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, monthsArray);
         monthsListView.setAdapter(arrayAdapter);
+
+        textboxTime1 = (EditText) findViewById(R.id.textbox_time1);
+        textboxTime2 = (EditText) findViewById(R.id.textbox_time2);
+        textboxTime3 = (EditText) findViewById(R.id.textbox_time3);
+        textboxTime4 = (EditText) findViewById(R.id.textbox_time4);
+
+        findViewById(R.id.button_SetSchedule).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                checkTime = textboxTime1.getText().toString();
+                if (!isValidEmail(checkTime)) {
+                    textboxTime1.setError("Invalid time, please use format 23:59");
+                }
+                checkTime = textboxTime2.getText().toString();
+                if (!isValidEmail(checkTime)) {
+                    textboxTime2.setError("Invalid time, please use format 23:59");
+                }
+                checkTime = textboxTime3.getText().toString();
+                if (!isValidEmail(checkTime)) {
+                    textboxTime3.setError("Invalid time, please use format 23:59");
+                }
+                checkTime = textboxTime4.getText().toString();
+                if (!isValidEmail(checkTime)) {
+                    textboxTime4.setError("Invalid time, please use format 23:59");
+                }
+
+
+            }
+        });
     }
 
+    private boolean isValidEmail(String time) {
+        String time_pattern = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+        Pattern pattern = Pattern.compile(time_pattern);
+        Matcher matcher = pattern.matcher(time);
+        return matcher.matches();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,10 +100,5 @@ public class timermenu extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goToTimerActivity(View view) {
-        Intent intent = new Intent(timermenu.this, Timer.class);
-        startActivity(intent);
     }
 }
